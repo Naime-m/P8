b5a8983ab37a1142033892184eb7764e54440aad
Naime Medjek <naimemedjek@gmail.com>
2022-03-30 12:41:12 +0200

    fixture install

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,38 +2,37 @@
 
 namespace Tests\App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-
     public function testHomepageIsUp()
     {
-        $client = static::createClient(array(), array(
-        'PHP_AUTH_USER' => 'test',
-        'PHP_AUTH_PW'   => 'test',
-    ));
-    $client->request('GET', '/');
-    $this->assertSame(200, $client->getResponse()->getStatusCode());
-    }
-
-    public function testHomepageRedirectIfNoUser()
-    {
         $client = static::createClient();
-    $client->request('GET', '/');
-    $this->assertTrue($client->getResponse()->isRedirect());
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('test@test.local');
+        $client->loginUser($testUser);
+        $client->request('GET', '/');
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
-    
-    
-    public function testHomepageShow()
-    {
-        $client = static::createClient(array(), array(
-            'PHP_AUTH_USER' => 'test',
-            'PHP_AUTH_PW'   => 'test',
-        ));
 
-       $crawler = $client->request('GET', '/');
+//     public function testHomepageRedirectIfNoUser()
+//     {
+//         $client = static::createClient();
+//     $client->request('GET', '/');
+//     $this->assertTrue($client->getResponse()->isRedirect());
+//     }
 
-       $this->assertGreaterThan(0, $crawler->filter('html:contains("Bienvenue sur Todo List")')->count());
-    }
+//     public function testHomepageShow()
+//     {
+//         $client = static::createClient(array(), array(
+//             'PHP_AUTH_USER' => 'test',
+//             'PHP_AUTH_PW'   => 'test',
+//         ));
+
+//        $crawler = $client->request('GET', '/');
+
+//        $this->assertGreaterThan(0, $crawler->filter('html:contains("Bienvenue sur Todo List")')->count());
+//     }
 }
